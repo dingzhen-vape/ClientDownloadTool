5b3116b199a65c95ee8e2786345fb8c77b5c72c0
顶针 <75886707+dingzhen-vape@users.noreply.github.com>
2025-04-28 18:16:46 +0800

    重构

--- main.py
+++ main.py
@@ -1,75 +1,75 @@
+
 import os
-import sys
-import requests
 import time
-from tkinter import Tk
 from tkinter.filedialog import asksaveasfilename
-import threading
+import json
+import requests
+import clsMenu
 
-event = threading.Event()
+class VersionGetTool:
+    def __init__(self):
+        MainMenu = clsMenu.SimpleMenu(ShowIndex=True).addOption("WurstCN",self.WurstCN).addOption("MeteorCN",self.Meteor)
+        MainMenu.addOption("刷新数据",self.RefreshInfo)
+        MainMenu.ShowMenu()
+    def RefreshInfo(self):
+        with open("./versions.json","w+",encoding="utf-8") as f:
+            Versions = {"WurstCN":self.GetVersions("WurstCN"), "MeteorCN":self.GetVersions("MeteorCN")}
+            json.dump(Versions,f,ensure_ascii=False,indent=4)
 
-def download_file(url, client):
-    try:
+    def Meteor(self):
+        MeteorMenu = clsMenu.SimpleMenu(ShowIndex=True)
+        with open("./versions.json","r",encoding="utf-8") as f:
+            Datas:dict[list,list,list,list] = json.load(f)["MeteorCN"]
+        for Version in Datas["Versions"]:
+            MeteorMenu.addOption(Version,lambda index = Datas["Versions"].index(Version) :self.DownLoadFile(Datas["DownloadUrls"][index],Datas["Versions"][index]))
+        MeteorMenu.addOption("返回上级菜单",MeteorMenu.Exit)
+
+        MeteorMenu.ShowMenu()
+
+    def WurstCN(self):
+        WurstMenu = clsMenu.SimpleMenu(ShowIndex=True)
+        with open("./versions.json","r",encoding="utf-8") as f:
+            Datas:dict[list,list,list,list] = json.load(f)["WurstCN"]
+        for Version in Datas["Versions"]:
+            WurstMenu.addOption(Version,lambda index = Datas["Versions"].index(Version) :self.DownLoadFile(Datas["DownloadUrls"][index],Datas["Versions"][index]))
+        WurstMenu.addOption("返回上级菜单",WurstMenu.Exit)
+
+        WurstMenu.ShowMenu()
+    
+    def DownLoadFile(self,Url,name):
+        os.system("cls")
+        print("正在获取文件...")
+        print(name,Url)
+        filePath = asksaveasfilename(initialfile=f"{name}", defaultextension=".jar", filetypes=[("JAR files", "*.jar"), ("All files", "*.*")])
+        if filePath == "":
+            return
+        with open(filePath, "wb") as f:
+            f.write(requests.get(Url).content)
+        print("保存成功")
+        time.sleep(3)
+    def GetVersions(self,Name):
+        url = f'https://api.github.com/repos/dingzhen-vape/{Name}/releases/tags/ATV'
         response = requests.get(url)
-    except requests.exceptions.SSLError:
-        print('SSL证书错误，请检查网络连接（vpn）')
-        for retry in range(1, 11):
-            try:
-                response = requests.get(url)
-            except:
-                print(f'失败,三秒后重试{retry}/10，检查网络连接（vpn）')
-                time.sleep(3)
-    if response.status_code == 200:
-        print('链接成功(若未弹出对话框，请重开程序)')
-        time.sleep(0.1)
-        save_path = asksaveasfilename(initialfile=f"{client}.jar", defaultextension=".jar", filetypes=[("JAR files", "*.jar"), ("All files", "*.*")])
-        time.sleep(0.1)
-        if save_path:
-            with open(save_path, "wb") as f:
-                f.write(response.content)
-            print('文件已保存到:', save_path)
+        if response.status_code == 200:
+            release = json.loads(response.content)["assets"]
+            Versions = []
+            DownloadCounts = []
+            DownloadUrls = []
+            UpdatedAt = []
+            for version in release:
+                Versions.append(version["name"])
+                DownloadCounts.append(version["download_count"])
+                DownloadUrls.append(version["browser_download_url"])
+                UpdatedAt.append(version["updated_at"])
+            os.system("cls")
+            print("获取成功")
+            time.sleep(3)
+            return {"Versions":Versions,"UpdatedAt":UpdatedAt,"DownloadUrls":DownloadUrls,"DownloadCounts":DownloadCounts}
         else:
-            print('用户取消了文件保存操作')
-    elif response.status_code == 404:
-        print("链接不存在，请检查版本号是否正确,或该版本尚未更新，敬请期待")
-    else:
-        print('无法获取数据，状态码:', response.status_code)
-        for retry in range(1, 11):
-            try:
-                response = requests.get(url)
-                if response.status_code == 200:
-                    print('链接成功')
-                    download_file(url, client)
-                    break
-            except:
-                print(f'失败,三秒后重试{retry}/10，检查网络连接（vpn）')
-                time.sleep(3)
+            os.system("cls")
+            print("获取版本信息失败，请检查网络连接或者更换网络环境")
+            print(response)
+            print(response.content)
+            time.sleep(5)
 
-    event.set()  # 设置事件标志，表示下载完成
-
-def main():
-    def demain():
-        version = input("请输入Minecraft版本号：")
-        WurstUrl = f'https://github.com/dingzhen-vape/WurstCN/releases/download/ATV/Wurst-ClientCN-MC{version}.jar'
-        MeteorUrl = f"https://github.com/dingzhen-vape/MeteorCN/releases/download/ATV/meteor-client-{version}.jar"
-        Client = input("请输入客户端名称（Wurst输入1/Meteor输入2）：")
-        if Client == "1":
-            print("正在获取链接，请等待（github），可能需要一点时间...")
-            root.after(0, download_file, WurstUrl, f"Wurst-ClientCN-MC{version}")
-        elif Client == "2":
-            print("正在获取链接，请等待（github），可能需要一点时间...")
-            root.after(0, download_file, MeteorUrl, f"meteor-client-{version}")
-        event.wait()
-        print("程序即将退出")
-        root.quit()  # 退出Tkinter主循环
-        time.sleep(5)
-        sys.exit()
-
-    global root
-    root = Tk()
-    root.withdraw()
-    threading.Thread(target=demain).start()
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
+a = VersionGetTool()
