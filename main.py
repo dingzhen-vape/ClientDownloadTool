11ba11edea7a4956cdd0272c7615c4cfd313f969
顶针 <75886707+dingzhen-vape@users.noreply.github.com>
2025-04-29 20:54:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 
+from encodings.punycode import T
+from http.cookies import SimpleCookie
 import os
 import time
 from tkinter.filedialog import asksaveasfilename
@@ -11,17 +13,22 @@ class VersionGetTool:
         MainMenu = clsMenu.SimpleMenu(ShowIndex=True).addOption("WurstCN",self.WurstCN).addOption("MeteorCN",self.Meteor)
         MainMenu.addOption("刷新数据",self.RefreshInfo)
         MainMenu.ShowMenu()
+
     def RefreshInfo(self):
-        with open("./versions.json","w+",encoding="utf-8") as f:
-            Versions = {"WurstCN":self.GetVersions("WurstCN"), "MeteorCN":self.GetVersions("MeteorCN")}
-            json.dump(Versions,f,ensure_ascii=False,indent=4)
+        MeteorDict = self.GetVersions("MeteorCN")
+        WurstDict = self.GetVersions("WurstCN")
+        if MeteorDict != {} and WurstDict != {}:
+            with open("./versions.json","w+",encoding="utf-8") as f:
+                Versions = {"WurstCN": WurstDict, "MeteorCN":MeteorDict}
+                json.dump(Versions,f,ensure_ascii=False,indent=4)
 
     def Meteor(self):
         MeteorMenu = clsMenu.SimpleMenu(ShowIndex=True)
         with open("./versions.json","r",encoding="utf-8") as f:
             Datas:dict[list,list,list,list] = json.load(f)["MeteorCN"]
         for Version in Datas["Versions"]:
-            MeteorMenu.addOption(Version,lambda index = Datas["Versions"].index(Version) :self.DownLoadFile(Datas["DownloadUrls"][index],Datas["Versions"][index]))
+            MeteorMenu.addOption(Version,
+                                 lambda index = Datas["Versions"].index(Version):self.ShowInfo(index,Datas) )
         MeteorMenu.addOption("返回上级菜单",MeteorMenu.Exit)
 
         MeteorMenu.ShowMenu()
@@ -31,22 +38,36 @@ class VersionGetTool:
         with open("./versions.json","r",encoding="utf-8") as f:
             Datas:dict[list,list,list,list] = json.load(f)["WurstCN"]
         for Version in Datas["Versions"]:
-            WurstMenu.addOption(Version,lambda index = Datas["Versions"].index(Version) :self.DownLoadFile(Datas["DownloadUrls"][index],Datas["Versions"][index]))
+            WurstMenu.addOption(Version,
+                                lambda index = Datas["Versions"].index(Version):self.ShowInfo(index,Datas))
         WurstMenu.addOption("返回上级菜单",WurstMenu.Exit)
 
         WurstMenu.ShowMenu()
     
+    def ShowInfo(self,index,Datas):
+        InfoMenu = clsMenu.SimpleMenu(OneTime=True)
+        InfoMenu.addOption(f"版本号:{Datas['Versions'][index]}")
+        InfoMenu.addOption(f"更新时间:{Datas['UpdatedAt'][index]}")
+        InfoMenu.addOption(f"下载次数:{Datas['DownloadCounts'][index]}")
+        InfoMenu.addOption(f"下载地址:{Datas['DownloadUrls'][index]}")
+        InfoMenu.addOption("下载",lambda:self.DownLoadFile(Datas["DownloadUrls"][index],Datas["Versions"][index]))
+        InfoMenu.addOption("返回上级菜单",InfoMenu.Exit)
+        InfoMenu.ShowMenu()
+
     def DownLoadFile(self,Url,name):
         os.system("cls")
         print("正在获取文件...")
         print(name,Url)
-        filePath = asksaveasfilename(initialfile=f"{name}", defaultextension=".jar", filetypes=[("JAR files", "*.jar"), ("All files", "*.*")])
+        filePath = asksaveasfilename(initialfile=f"{name}",
+                                        defaultextension=".jar",
+                                        filetypes=[("JAR files", "*.jar"), ("All files", "*.*")])
         if filePath == "":
             return
         with open(filePath, "wb") as f:
             f.write(requests.get(Url).content)
         print("保存成功")
         time.sleep(3)
+
     def GetVersions(self,Name):
         url = f'https://api.github.com/repos/dingzhen-vape/{Name}/releases/tags/ATV'
         response = requests.get(url)
